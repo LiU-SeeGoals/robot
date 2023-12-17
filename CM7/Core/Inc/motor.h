228b7086999b3bb2ad5fb83d604c3d53f40e21d9
rwn <contact@0x57.me>
2023-12-17 16:30:37 +0100

    update according to fw standard

--- CM7/Core/Inc/motor.h
+++ CM7/Core/Inc/motor.h
@@ -1,22 +1,59 @@
 #ifndef __MOTOR_H
 #define __MOTOR_H
 
-typedef struct 
+
+/* Public includes */
+#include "main.h"
+
+
+/* Public structs */
+
+/**
+ * Describe this
+ */
+typedef struct
 {
-    TIM_HandleTypeDef *htim;
-    uint32_t channel;
-    uint16_t reversePin;
-    GPIO_TypeDef reversePinPort;
-    uint16_t breakPin;
-    GPIO_TypeDef breakPinPort;
-    uint16_t readSpeedPin;
-    GPIO_TypeDef readSpeedPinPort;
-    uint16_t reversing;
+  TIM_HandleTypeDef *htim;
+  uint32_t channel;
+  uint16_t reversePin;
+  GPIO_TypeDef reversePinPort;
+  uint16_t breakPin;
+  GPIO_TypeDef breakPinPort;
+  uint16_t readSpeedPin;
+  GPIO_TypeDef readSpeedPinPort;
+  uint16_t reversing;
 } MotorPWM;
 
-void startMotor(MotorPWM *motor);
-void setSpeed(MotorPWM *motor, float percent);
-void breakMotor(MotorPWM *motor);
-int readSpeed(MotorPWM *motor);
 
-#endif
+/* Public function declarations */
+
+/**
+ * Describe this
+ *
+ * @param motor Describe this
+ */
+void MOTOR_StartMotor(MotorPWM *motor);
+
+/**
+ * Describe this
+ *
+ * @param motor Describe this
+ * @param percent Describe this
+ */
+void MOTOR_SetSpeed(MotorPWM *motor, float percent);
+
+/**
+ * Describe this
+ *
+ * @param motor Describe this
+ */
+void MOTOR_BreakMotor(MotorPWM *motor);
+
+/**
+ * Describe this
+ *
+ * @param motor Describe this
+ */
+int MOTOR_ReadSpeed(MotorPWM *motor);
+
+#endif /* __MOTOR_H */
