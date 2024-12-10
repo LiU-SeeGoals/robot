7f99bc90a6d21e739cbe82b74aadc141194b5e89
Henrik Lindgren <henrik@hlindgren.xyz>
2024-12-10 08:28:24 +0100

    uncomment UI_Init() call

--- CM7/Core/Src/main.c
+++ CM7/Core/Src/main.c
@@ -192,7 +192,7 @@ Error_Handler();
   HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET);
   HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
   LOG_INFO("Startup finished...\r\n");
-  /* UI_Init(&huart3); */
+  UI_Init(&huart3);
   /* USER CODE END 2 */
 
   /* Infinite loop */
