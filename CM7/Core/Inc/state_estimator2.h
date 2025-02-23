1b64aef96a1b8453b14d77210a733052e1522ef7
Anton <anton.ostman123@gmail.com>
2025-02-23 22:06:36 +0100

    Add new state estimators

--- CM7/Core/Inc/state_estimator2.h
+++ CM7/Core/Inc/state_estimator2.h
@@ -1,2 +1,179 @@
+#ifndef STATE_ESTIMATOR2_H
+#define STATE_ESTIMATOR2_H
+
+#include "arm_math.h"
+#include "imu.h"
+#include "lag_element.h"
+
+/*
+	// EKF state vector: [ px py pw vx vy ]
+	// input vector: [ gyr_w acc_x acc_y ]
+	// measurement vector: [ px py pw ]
+  
+  Input enters through the prediction update and
+  measurements enter via measurement update.
+
+  GLOBAL FRAME COORDINATES (Football field): px, py, pw
+  ROBOT FRAME COORDINATES (Center of robot seen from above): vx, vy
+  IMU FRAME COORDINATES: (Center of imu seen from above) gyr_w, acc_x, acc_y
+  // NOTE: IMU might not be perfectly aligned with the robot,
+  // estimating the orientation of IMU will likely improve estimates
+
+*/
+
+#define EKF_SIZE_A(f)		(f*f)
+#define EKF_SIZE_C(h,f)		(h*f)
+#define EKF_SIZE_EX(f)		(f*f)
+#define EKF_SIZE_EZ(h)		(h*h)
+#define EKF_SIZE_X(f)		(f)
+#define EKF_SIZE_SIGMA(f)	(f*f)
+#define EKF_SIZE_K(f,h)		(f*h)
+#define EKF_SIZE_U(g)		(g)
+#define EKF_SIZE_Z(h)		(h)
+#define EKF_SIZE_MAX(max)	(max*max)
+
+// state vector (x) rows: f
+// control vector (u) rows: g
+// sensor vector (z) rows: h
+#define EKF_DATA_SIZE(f,g,h,max) \
+	(EKF_SIZE_A(f)+EKF_SIZE_C(h,f)+EKF_SIZE_EX(f) \
+	+EKF_SIZE_EZ(h)+EKF_SIZE_X(f)+EKF_SIZE_SIGMA(f)+EKF_SIZE_K(f,h)+EKF_SIZE_U(g) \
+	+EKF_SIZE_Z(h)+EKF_SIZE_MAX(max)*3)
+
+#define MAT_ELEMENT(mat,r,c) ((mat).pData[(mat).numCols*(r)+(c)])
+
+typedef void(*EKFStateFunc)(arm_matrix_instance_f32* pX, const arm_matrix_instance_f32* pU);
+typedef void(*EKFStateJacobianFunc)(const arm_matrix_instance_f32* pX, const arm_matrix_instance_f32* pU, arm_matrix_instance_f32* pF);
+typedef void(*EKFMeasFunc)(const arm_matrix_instance_f32* pX, arm_matrix_instance_f32* pY);
+typedef void(*EKFMeasJacobianFunc)(const arm_matrix_instance_f32* pX, arm_matrix_instance_f32* pH);
+
+typedef struct {
+
+	uint16_t f;
+	uint16_t g;
+	uint16_t h;
+
+	// state and measurement functions
+	EKFStateFunc pState;
+	EKFStateJacobianFunc pStateJacobian;
+	EKFMeasFunc pMeas;
+	EKFMeasJacobianFunc pMeasJacobian;
+
+	// user matrices
+	arm_matrix_instance_f32 A;		// (f x f)
+	arm_matrix_instance_f32 C;		// (h x f)
+	arm_matrix_instance_f32 Ex;		// (f x f)
+	arm_matrix_instance_f32 Ez;		// (h x h)
+
+	// internal matrices
+	arm_matrix_instance_f32 x;		// state (f x 1)
+	arm_matrix_instance_f32 Sigma;	// uncertainty (f x f)
+	arm_matrix_instance_f32 K;		// Kalman gain (f x h)
+
+	// command input
+	arm_matrix_instance_f32 u;		// (g x 1)
+
+	// sensor input
+	arm_matrix_instance_f32 z;		// (h x 1)
+
+	// temporary calculation matrices
+	arm_matrix_instance_f32 tmp1;
+	arm_matrix_instance_f32 tmp2;
+	arm_matrix_instance_f32 tmp3;
+} robot_state; 
+
+
+typedef struct _EKF
+
+{
+	uint16_t f;
+	uint16_t g;
+	uint16_t h;
+
+	// state and measurement functions
+	EKFStateFunc pState;
+	EKFStateJacobianFunc pStateJacobian;
+	EKFMeasFunc pMeas;
+	EKFMeasJacobianFunc pMeasJacobian;
+
+	// user matrices
+	arm_matrix_instance_f32 A;		// (f x f)
+	arm_matrix_instance_f32 C;		// (h x f)
+	arm_matrix_instance_f32 Ex;		// (f x f)
+	arm_matrix_instance_f32 Ez;		// (h x h)
+
+	// internal matrices
+	arm_matrix_instance_f32 x;		// state (f x 1)
+	arm_matrix_instance_f32 Sigma;	// uncertainty (f x f)
+	arm_matrix_instance_f32 K;		// Kalman gain (f x h)
+
+	// command input
+	arm_matrix_instance_f32 u;		// (g x 1)
+
+	// sensor input
+	arm_matrix_instance_f32 z;		// (h x 1)
+
+	// temporary calculation matrices
+	arm_matrix_instance_f32 tmp1;
+	arm_matrix_instance_f32 tmp2;
+	arm_matrix_instance_f32 tmp3;
+} EKF;
+
+typedef struct _FusionEKF
+{
+	// EKF state vector: [ px py pw vx vy ]
+	// input vector: [ gyr_w acc_x acc_y ]
+	// measurement vector: [ px py pw ]
+	EKF ekf;
+	float ekfData[EKF_DATA_SIZE(5, 3, 3, 5)];
+
+	/*float encGyrPos[3];*/
+
+	struct
+	{
+		uint16_t online;
+		/*uint32_t timeLastValidSample;*/
+		/*int32_t turns;*/
+		/*float lastOrient;*/
+		/*uint32_t numLateMeasurements;*/
+	} vision;
+
+	/*FusionEKFConfig* pConfig;*/
+
+	/*FusionEKFTimeSlot timeSlots[FUSION_EKF_MAX_DELAY];*/
+	/*uint32_t timeSlotNow;*/
+
+	/*ModelEnc modelEnc;*/
+
+	LagElementPT1 lagAccel[2];
+
+	/*struct*/
+	/*{*/
+	/*	LagElementPT1 lagCurrent;*/
+	/**/
+	/*	uint8_t isStrongDribbling;*/
+	/*	uint32_t strongTicks;*/
+	/**/
+	/*	FusionEKFDribblerIdleCurTable idleCurrentTable;*/
+	/*	FlashFile* pIdleCurrentFile;*/
+	/*} dribbler;*/
+
+	/*float ballPosGlobal[2];*/
+	/*float ballVelGlobal[2];*/
+	/*uint32_t ballLastDetectedTimestamp;*/
+} FusionEKF;
+
+
 void STATE_Init();
 void STATE_Test();
+void STATE_FusionEKFVisionUpdate(float posx, float posy, float posw);
+void STATE_FusionEKFIntertialUpdate(IMU_AccelVec3 acc, IMU_GyroVec3 gyr, float gyrbiasz, float accbiasx, float accbiasy);
+
+static void ekfStateJacobianFunc(const arm_matrix_instance_f32* pX, const arm_matrix_instance_f32* pU, arm_matrix_instance_f32* pF);
+static void ekfMeasFunc(const arm_matrix_instance_f32* pX, arm_matrix_instance_f32* pY);
+static void ekfStateFunc(arm_matrix_instance_f32* pX, const arm_matrix_instance_f32* pU);
+static void ekfMeasJacobianFunc(const arm_matrix_instance_f32* pX, arm_matrix_instance_f32* pH);
+static void EKFInit(EKF* pKF, uint16_t numStates, uint16_t numCtrl, uint16_t numSensors, float* pData);
+static void initEKF();
+
+#endif
--- CM7/Core/Src/state_estimator2.c
+++ CM7/Core/Src/state_estimator2.c
@@ -1,9 +1,20 @@
 #include "arm_math.h"
+#include "arm_mat_util_f32.h"
 #include "state_estimator2.h"
+#include "lag_element.h"
+#include "imu.h"
 
+#include <string.h>
 #include "log.h"
 
+#define CTRL_DELTA_T 0.0001
+
 static LOG_Module internal_log_mod;
+
+
+// NOTE: Assume 1000hz loop
+
+FusionEKF fusionEKF;
 
 const float32_t A_f32[16] =
 {
@@ -22,13 +33,215 @@ const float32_t B_f32[16] =
   0.0,     0.0,     0.0,     1.0,
 };
 
+// Improvements: Detect when vision loses us and rely on encoders
 
 /*
  *  Init logging for state
  */
 
+void EKFPredict(EKF* pKF)
+{
+	// >>> State prediction
+	// A = jacobian(x+1,x)
+	(*pKF->pStateJacobian)(&pKF->x, &pKF->u, &pKF->A);
+	(*pKF->pState)(&pKF->x, &pKF->u);
+
+	// >>> Sigma = A*Sigma*A^T + Ex
+	// tmp1 = A*Sigma (f x f)
+	pKF->tmp1.numRows = pKF->f;
+	pKF->tmp1.numCols = pKF->f;
+	arm_mat_mult_f32(&pKF->A, &pKF->Sigma, &pKF->tmp1);
+	// tmp2 = A^T (f x f)
+	pKF->tmp2.numRows = pKF->f;
+	pKF->tmp2.numCols = pKF->f;
+	arm_mat_trans_f32(&pKF->A, &pKF->tmp2);
+	// Sigma = tmp1*tmp2 (f x f)
+	arm_mat_mult_f32(&pKF->tmp1, &pKF->tmp2, &pKF->Sigma);
+	// Sigma = Sigma + Ex (f x f)
+	arm_mat_add_f32(&pKF->Sigma, &pKF->Ex, &pKF->Sigma);
+}
+
+void EKFUpdate(EKF* pKF)
+{
+	// >>> H = jacobian(z,x)
+	(*pKF->pMeasJacobian)(&pKF->x, &pKF->C);
+
+	// >>> K = Sigma*C^T*(C*Sigma*C^T+Ez)^-1
+	// tmp1 = C^T (f x h)
+	pKF->tmp1.numRows = pKF->f;
+	pKF->tmp1.numCols = pKF->h;
+	arm_mat_trans_f32(&pKF->C, &pKF->tmp1);
+	// tmp2 = Sigma*tmp1 (f x h)
+	pKF->tmp2.numRows = pKF->f;
+	pKF->tmp2.numCols = pKF->h;
+	arm_mat_mult_f32(&pKF->Sigma, &pKF->tmp1, &pKF->tmp2);
+	// tmp3 = C*tmp2 (h x h)
+	pKF->tmp3.numRows = pKF->h;
+	pKF->tmp3.numCols = pKF->h;
+	arm_mat_mult_f32(&pKF->C, &pKF->tmp2, &pKF->tmp3);
+	// tmp3 = tmp3+Ez (h x h)
+	arm_mat_add_f32(&pKF->tmp3, &pKF->Ez, &pKF->tmp3);
+	// tmp1 = tmp3^-1 (h x h)
+	pKF->tmp1.numRows = pKF->h;
+	pKF->tmp1.numCols = pKF->h;
+	arm_status invStat;
+	switch(pKF->h)
+	{
+		case 1:
+			if(pKF->tmp3.pData[0] == 0.0f)
+			{
+				invStat = ARM_MATH_SINGULAR;
+			}
+			else
+			{
+				pKF->tmp1.pData[0] = 1.0f/pKF->tmp3.pData[0];
+				invStat = ARM_MATH_SUCCESS;
+			}
+			break;
+		case 2:
+			invStat = arm_mat_inv_2x2_f32(&pKF->tmp3, &pKF->tmp1);
+			break;
+		case 3:
+			invStat = arm_mat_inv_3x3_f32(&pKF->tmp3, &pKF->tmp1);
+			break;
+		default:
+			invStat = arm_mat_inverse_f32(&pKF->tmp3, &pKF->tmp1);
+			break;
+	}
+	/*if(invStat != ARM_MATH_SUCCESS)*/
+	/*	LogErrorC("Matrix inverse error", invStat);*/
+	// K = tmp2*tmp1 (f x h)
+	arm_mat_mult_f32(&pKF->tmp2, &pKF->tmp1, &pKF->K);
+
+	// >>> x = x + K*(z - pMeas(x))
+	// tmp1 = pMeas(x) (h x 1)
+	pKF->tmp1.numRows = pKF->h;
+	pKF->tmp1.numCols = 1;
+	(*pKF->pMeas)(&pKF->x, &pKF->tmp1);
+	// tmp2 = z - tmp1 (h x 1)
+	pKF->tmp2.numRows = pKF->h;
+	pKF->tmp2.numCols = 1;
+	arm_mat_sub_f32(&pKF->z, &pKF->tmp1, &pKF->tmp2);
+	// tmp3 = K*tmp2 (f x 1)
+	pKF->tmp3.numRows = pKF->f;
+	pKF->tmp3.numCols = 1;
+	arm_mat_mult_f32(&pKF->K, &pKF->tmp2, &pKF->tmp3);
+	// x = x + tmp3 (f x 1)
+	arm_mat_add_f32(&pKF->x, &pKF->tmp3, &pKF->x);
+
+	// >>> Sigma = (I - K*C)*Sigma
+	// tmp1 = K*C (f x f)
+	pKF->tmp1.numRows = pKF->f;
+	pKF->tmp1.numCols = pKF->f;
+	arm_mat_mult_f32(&pKF->K, &pKF->C, &pKF->tmp1);
+	// tmp2 = I (f x f)
+	pKF->tmp2.numRows = pKF->f;
+	pKF->tmp2.numCols = pKF->f;
+	arm_mat_identity_f32(&pKF->tmp2);
+	// tmp2 = tmp2 - tmp1 (f x f)
+	arm_mat_sub_f32(&pKF->tmp2, &pKF->tmp1, &pKF->tmp2);
+	// tmp3 = Sigma (f x f)
+	arm_mat_copy_f32(&pKF->Sigma, &pKF->tmp3);
+	// Sigma = tmp2*tmp3 (f x f)
+	arm_mat_mult_f32(&pKF->tmp2, &pKF->tmp3, &pKF->Sigma);
+}
+
+void EKFInit(EKF* pKF, uint16_t numStates, uint16_t numCtrl, uint16_t numSensors, float* pData)
+{
+	uint16_t f = numStates;
+	uint16_t g = numCtrl;
+	uint16_t h = numSensors;
+
+	uint16_t max = f;
+	if(g > max)
+		max = g;
+	if(h > max)
+		max = h;
+
+	memset(pData, 0, EKF_DATA_SIZE(f, g, h, max)*sizeof(float));
+
+	pKF->f = f;
+	pKF->g = g;
+	pKF->h = h;
+
+	pKF->A.numRows = f;
+	pKF->A.numCols = f;
+	pKF->A.pData = pData;
+	pData += EKF_SIZE_A(f);
+
+	pKF->C.numRows = h;
+	pKF->C.numCols = f;
+	pKF->C.pData = pData;
+	pData += EKF_SIZE_C(h, f);
+
+	pKF->Ex.numRows = f;
+	pKF->Ex.numCols = f;
+	pKF->Ex.pData = pData;
+	pData += EKF_SIZE_EX(f);
+
+	pKF->Ez.numRows = h;
+	pKF->Ez.numCols = h;
+	pKF->Ez.pData = pData;
+	pData += EKF_SIZE_EZ(h);
+
+	pKF->x.numRows = f;
+	pKF->x.numCols = 1;
+	pKF->x.pData = pData;
+	pData += EKF_SIZE_X(f);
+
+	pKF->Sigma.numRows = f;
+	pKF->Sigma.numCols = f;
+	pKF->Sigma.pData = pData;
+	pData += EKF_SIZE_SIGMA(f);
+
+	pKF->K.numRows = f;
+	pKF->K.numCols = h;
+	pKF->K.pData = pData;
+	pData += EKF_SIZE_K(f,h);
+
+	pKF->u.numRows = g;
+	pKF->u.numCols = 1;
+	pKF->u.pData = pData;
+	pData += EKF_SIZE_U(g);
+
+	pKF->z.numRows = h;
+	pKF->z.numCols = 1;
+	pKF->z.pData = pData;
+	pData += EKF_SIZE_Z(h);
+
+	pKF->tmp1.numRows = max;
+	pKF->tmp1.numCols = max;
+	pKF->tmp1.pData = pData;
+	pData += EKF_SIZE_MAX(max);
+
+	pKF->tmp2.numRows = max;
+	pKF->tmp2.numCols = max;
+	pKF->tmp2.pData = pData;
+	pData += EKF_SIZE_MAX(max);
+
+	pKF->tmp3.numRows = max;
+	pKF->tmp3.numCols = max;
+	pKF->tmp3.pData = pData;
+	pData += EKF_SIZE_MAX(max);
+
+	pData += EKF_SIZE_X(f);
+
+	arm_mat_identity_f32(&pKF->Sigma);
+}
+
+
 void STATE_Init(){
+
   LOG_InitModule(&internal_log_mod, "STATE", LOG_LEVEL_TRACE);
+	initEKF();
+
+	/*LagElementPT1Init(&fusionEKF.lagAccel[0], 1.0f, fusionEKF.pConfig->emaAccelT, CTRL_DELTA_T);*/
+	/*LagElementPT1Init(&fusionEKF.lagAccel[1], 1.0f, fusionEKF.pConfig->emaAccelT, CTRL_DELTA_T);*/
+
+	LagElementPT1Init(&fusionEKF.lagAccel[0], 1.0f, 0.001, CTRL_DELTA_T);
+	LagElementPT1Init(&fusionEKF.lagAccel[1], 1.0f, 0.001, CTRL_DELTA_T);
+
+	/*LagElementPT1Init(&fusionEKF.dribbler.lagCurrent, 1.0f, 0.005f, CTRL_DELTA_T);*/
 }
 
 /*
@@ -41,6 +254,155 @@ void STATE_logm44(const float32_t* m44){
       m44[2*4], m44[2*4 + 1], m44[2*4 + 2], m44[2*4 + 3],
       m44[3*4], m44[3*4 + 1], m44[3*4 + 2], m44[3*4 + 3]
       );
+}
+
+/*
+   Calculate the jacobian, mostly used to update covariance.
+*/
+
+static void ekfStateJacobianFunc(const arm_matrix_instance_f32* pX, const arm_matrix_instance_f32* pU, arm_matrix_instance_f32* pF)
+{
+	float gyr_w = MAT_ELEMENT(*pU, 0, 0);
+	float acc_x = MAT_ELEMENT(*pU, 1, 0);
+	float acc_y = MAT_ELEMENT(*pU, 2, 0);
+
+	float p_w = MAT_ELEMENT(*pX, 2, 0);
+	float v_x = MAT_ELEMENT(*pX, 3, 0);
+	float v_y = MAT_ELEMENT(*pX, 4, 0);
+
+  /*float SF1 = gyr_w/2000.0f + p_w - M_PI_2;*/
+  float SF1 = 3.14/2.0;
+	float SF2 = arm_cos_f32(SF1)/1000.0f + (gyr_w * arm_sin_f32(SF1))/2000000.0f;
+	float SF3 = (gyr_w * arm_cos_f32(SF1))/2000000.0f;
+	float SF4 = arm_sin_f32(SF1);
+	float SF5 = acc_y - gyr_w * v_x;
+	float SF6 = arm_cos_f32(SF1);
+	float SF7 = gyr_w/1000.0f;
+
+	arm_mat_identity_f32(pF);
+
+	MAT_ELEMENT(*pF, 0, 2) = - (SF5*SF6)/2000000.0f - (v_x*SF4)/1000.0f - (v_y*SF6)/1000.0f - (SF4*(acc_x + gyr_w*v_y))/2000000.0f;
+	MAT_ELEMENT(*pF, 0, 3) = SF2;
+	MAT_ELEMENT(*pF, 0, 4) = SF3 - SF4/1000.0f;
+
+	MAT_ELEMENT(*pF, 1, 2) = (v_x*SF6)/1000.0f - (SF4*SF5)/2000000.0f - (v_y*SF4)/1000.0f + (SF6*(acc_x + gyr_w*v_y))/2000000.0f;
+	MAT_ELEMENT(*pF, 1, 3) = SF4/1000.0f - SF3;
+	MAT_ELEMENT(*pF, 1, 4) = SF2;
+
+	MAT_ELEMENT(*pF, 3, 4) = SF7;
+
+	MAT_ELEMENT(*pF, 4, 3) = -SF7;
+}
+
+static void ekfMeasFunc(const arm_matrix_instance_f32* pX, arm_matrix_instance_f32* pY)
+{
+	memcpy(pY->pData, pX->pData, sizeof(float)*3);
+}
+
+void ekfMeasJacobianFunc(const arm_matrix_instance_f32* pX, arm_matrix_instance_f32* pH)
+{
+	(void)pX;
+
+	arm_mat_zero_f32(pH);
+	MAT_ELEMENT(*pH, 0, 0) = 1.0f;
+	MAT_ELEMENT(*pH, 1, 1) = 1.0f;
+	MAT_ELEMENT(*pH, 2, 2) = 1.0f;
+}
+
+static void ekfStateFunc(arm_matrix_instance_f32* pX, const arm_matrix_instance_f32* pU)
+{
+	const float dt = 0.001f;
+
+	float gyr_w = MAT_ELEMENT(*pU, 0, 0);
+	float acc_x = MAT_ELEMENT(*pU, 1, 0);
+	float acc_y = MAT_ELEMENT(*pU, 2, 0);
+
+	float p_x = MAT_ELEMENT(*pX, 0, 0);
+	float p_y = MAT_ELEMENT(*pX, 1, 0);
+	float p_w = MAT_ELEMENT(*pX, 2, 0);
+	float v_x = MAT_ELEMENT(*pX, 3, 0);
+	float v_y = MAT_ELEMENT(*pX, 4, 0);
+
+	float v_w = gyr_w;
+	/*float a = -M_PI_2 + p_w + dt*v_w*0.5f;*/
+	float angle = 0;
+	float a_x = acc_x + v_y*v_w;
+	float a_y = acc_y - v_x*v_w;
+
+	float px1 = p_x + (arm_cos_f32(angle)*v_x-arm_sin_f32(angle)*v_y)*dt + (arm_cos_f32(angle)*a_x-arm_sin_f32(angle)*a_y)*0.5f*dt*dt;
+	float py1 = p_y + (arm_sin_f32(angle)*v_x+arm_cos_f32(angle)*v_y)*dt + (arm_sin_f32(angle)*a_x+arm_cos_f32(angle)*a_y)*0.5f*dt*dt;
+	float vx1 = v_x + a_x*dt;
+	float vy1 = v_y + a_y*dt;
+	float pw1 = p_w + v_w*dt;
+
+	MAT_ELEMENT(*pX, 0, 0) = px1;
+	MAT_ELEMENT(*pX, 1, 0) = py1;
+	MAT_ELEMENT(*pX, 2, 0) = pw1;
+	MAT_ELEMENT(*pX, 3, 0) = vx1;
+	MAT_ELEMENT(*pX, 4, 0) = vy1;
+}
+
+static void initEKF()
+{
+	EKFInit(&fusionEKF.ekf, 5, 3, 3, fusionEKF.ekfData);
+	arm_mat_scale_f32(&fusionEKF.ekf.Sigma, 0.001f, &fusionEKF.ekf.Sigma);
+	fusionEKF.ekf.pState = &ekfStateFunc;
+	fusionEKF.ekf.pStateJacobian = &ekfStateJacobianFunc;
+	fusionEKF.ekf.pMeas = &ekfMeasFunc;
+	fusionEKF.ekf.pMeasJacobian = &ekfMeasJacobianFunc;
+
+	arm_mat_identity_f32(&fusionEKF.ekf.Ex);
+	arm_mat_identity_f32(&fusionEKF.ekf.Ez);
+
+  // TODO: Load some covariance values for process and measurement noise
+  // Instead of having idenity matrix
+	/*loadNoiseCovariancesFromConfig();*/
+}
+
+void STATE_FusionEKFVisionUpdate(float posx, float posy, float posw)
+{
+	// VISION
+
+  float pos[3] = {posx, posy, posw};
+
+  if(!fusionEKF.vision.online)
+  {
+    fusionEKF.vision.online = 1;
+
+    // Make sure EKF jumps immediately to new position in first measurement.
+
+    // TODO: We can be smart here and track when vision loses 
+    // the robot and rely on encoders instead (as tigers do hehe).
+    memcpy(fusionEKF.ekf.x.pData, pos, sizeof(float)*3);
+  }
+  else
+  {
+    // Move vision data to ekf measurement vector and do the measurement update
+    memcpy(fusionEKF.ekf.z.pData, pos, sizeof(float)*3);
+    EKFUpdate(&fusionEKF.ekf);
+  }
+}
+
+/*
+   Fuse IMU measurements.
+*/
+
+void STATE_FusionEKFIntertialUpdate(IMU_AccelVec3 acc, IMU_GyroVec3 gyr, float gyrbiasz, float accbiasx, float accbiasy)
+{
+
+	// GYRO + ACCELEROMETER
+  const float linear_acc_x = acc.x - accbiasx;
+  const float linear_acc_y = acc.y - accbiasy;
+
+  float gyrAcc[3];
+	gyrAcc[0] = gyr.z - gyrbiasz;
+  // Lowpass noisy accelerometer
+	gyrAcc[1] = LagElementPT1Process(&fusionEKF.lagAccel[0], linear_acc_x);
+	gyrAcc[2] = LagElementPT1Process(&fusionEKF.lagAccel[1], linear_acc_y);
+
+	// INERTIAL NAVIGATION SYSTEM (INS)
+	memcpy(fusionEKF.ekf.u.pData, gyrAcc, sizeof(float)*3);
+	EKFPredict(&fusionEKF.ekf);
 }
 
 void STATE_Test(){
@@ -69,7 +431,62 @@ void STATE_Test(){
     test_status = ARM_MATH_TEST_FAILURE;
   }
 
+  if (test_status != ARM_MATH_SUCCESS){
+    LOG_DEBUG("Test %d failed glhf\r\n", 1);
+  }
+
+  // More tests here.....
+
+  // This test is tricky because of the low pass filter... Seems to work though
+	initEKF();
+  IMU_AccelVec3 acc = {0,1,0};
+  IMU_GyroVec3 gyr = {0,0,0};
+  STATE_FusionEKFIntertialUpdate(acc,gyr,0,0,0);
+
+  float posx = MAT_ELEMENT(fusionEKF.ekf.x, 0, 0);
+  float posy = MAT_ELEMENT(fusionEKF.ekf.x, 1, 0);
+  float posw = MAT_ELEMENT(fusionEKF.ekf.x, 2, 0);
+  float velx = MAT_ELEMENT(fusionEKF.ekf.x, 3, 0);
+  float vely = MAT_ELEMENT(fusionEKF.ekf.x, 4, 0);
+  
+
+  LOG_DEBUG("Got from fusion (px,py,pw,vx,vy) %f %f %f %f %f\r\n", posx, posy, posw, velx, vely);
+
+  
+  
   if (test_status == ARM_MATH_SUCCESS){
     LOG_DEBUG("Tjoho all test passed\r\n");
   }
+
+}
+
+
+float STATE_get_posx(){
+  return MAT_ELEMENT(fusionEKF.ekf.x, 0, 0);
+}
+
+float STATE_get_posy(){
+  return MAT_ELEMENT(fusionEKF.ekf.x, 1, 0);
+}
+
+float STATE_get_robot_angle() {
+
+  // Might not need ?
+  /*if (robot.statew.X < 0){*/
+  /*  robot.statew.X += 2 * PI;*/
+  /*}*/
+  /*if (robot.statew.X > 2 * PI){*/
+  /*  robot.statew.X -= 2 * PI;*/
+  /*}*/
+  /*return robot.statew.X;*/
+
+  return MAT_ELEMENT(fusionEKF.ekf.x, 2, 0);
+}
+
+float STATE_get_vx(){
+  return MAT_ELEMENT(fusionEKF.ekf.x, 3, 0);
+}
+
+float STATE_get_vy(){
+  return MAT_ELEMENT(fusionEKF.ekf.x, 4, 0);
 }
